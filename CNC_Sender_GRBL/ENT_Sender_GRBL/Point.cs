fef3eac21f34b8580b1d691d08d3bec0fe2ac241
grodriguez1950 <gonzalo.rodriguez@1950labs.com>
2020-08-08 16:52:04 -0300

    no message

---
+++ CNC_Sender_GRBL/ENT_Sender_GRBL/Point.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENT_Sender_GRBL
+{
+    public class Point
+    {
+        public decimal X { get; set; }
+        public decimal Y { get; set; }
+        public decimal Z { get; set; }
+    }
+}
