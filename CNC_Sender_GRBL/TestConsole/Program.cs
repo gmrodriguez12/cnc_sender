32659c3b9dd3351376b9b36c15c666daabda3b47
grodriguez1950 <gonzalo.rodriguez@1950labs.com>
2020-08-12 16:36:46 -0300

    Testing Console

---
+++ CNC_Sender_GRBL/TestConsole/Program.cs
@@ -0,0 +1,41 @@
+﻿using BLL_Sender_GRBL;
+using ENT_Sender_GRBL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static ENT_Sender_GRBL.Enum.EnumHelpers;
+
+namespace TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Line lineDefinition = new Line()
+            {
+                Start = new Point()
+                {
+                    X = 2,
+                    Y = 3,
+                    Z = 1
+                },
+                Feed = 150,
+                Finish = new Point()
+                {
+                    X = 5,
+                    Y = 7,
+                    Z = 1
+                },
+                SafetyHeightZ = 10
+            };         
+
+            var geometric = GCodeFactory.Build((short)TypeGeometric.Line);
+            StringBuilder sb = geometric.GenerateSimulatorGCode(lineDefinition);
+
+            Console.WriteLine(sb.ToString());
+            Console.ReadLine();
+        }
+    }
+}
