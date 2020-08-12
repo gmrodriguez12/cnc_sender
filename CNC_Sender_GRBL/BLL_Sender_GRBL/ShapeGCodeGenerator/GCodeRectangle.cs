48bc27c855fc7830e8ff4bb181cd409e82eecbbf
grodriguez1950 <gonzalo.rodriguez@1950labs.com>
2020-08-12 17:56:48 -0300

    gcode rectangle

--- CNC_Sender_GRBL/BLL_Sender_GRBL/ShapeGCodeGenerator/GCodeRectangle.cs
+++ CNC_Sender_GRBL/BLL_Sender_GRBL/ShapeGCodeGenerator/GCodeRectangle.cs
@@ -11,20 +11,43 @@ namespace BLL_Sender_GRBL.ShapeGCodeGenerator
     {
         public override StringBuilder GenerateGCode(Geometric shape)
         {
-            /*
-             G0 X2 Y2
-                G1 X2 Y6 F150
-                G1 X4 Y6 F150
-                G1 X4 Y2 F150
-                G1 X2 Y2 F150
-                G0 X0 Y0
-             */
-            throw new NotImplementedException();
+            Rectangle rectangle = (Rectangle)shape;
+            return GenerateGcode(rectangle, true);
         }
 
         public override StringBuilder GenerateSimulatorGCode(Geometric shape)
         {
-            throw new NotImplementedException();
+            Rectangle rectangle = (Rectangle)shape;
+            return GenerateGcode(rectangle, true);
         }
+
+        private StringBuilder GenerateGcode(Rectangle rectangle, bool isSimulator)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(GMovement(rectangle.Start, "G0"));
+
+            if (!isSimulator)
+                sb.AppendLine(ToggleRele(true));
+
+            sb.AppendLine(GMovement(rectangle.A, rectangle.Feed, "G1"));
+            sb.AppendLine(GMovement(rectangle.B, rectangle.Feed, "G1"));
+            sb.AppendLine(GMovement(rectangle.C, rectangle.Feed, "G1"));
+            sb.AppendLine(GMovement(rectangle.Start, rectangle.Feed, "G1"));
+
+            if (!isSimulator)
+                sb.AppendLine(ToggleRele(false));
+
+            sb.AppendLine(ReturnToHome(rectangle.SafetyHeightZ));
+            return sb;
+        }
+
+        /*
+            G0 X2 Y2
+            G1 X2 Y6 F150
+            G1 X4 Y6 F150
+            G1 X4 Y2 F150
+            G1 X2 Y2 F150
+            G0 X0 Y0
+         */
     }
 }
--- CNC_Sender_GRBL/ENT_Sender_GRBL/Rectangle.cs
+++ CNC_Sender_GRBL/ENT_Sender_GRBL/Rectangle.cs
@@ -8,7 +8,40 @@ namespace ENT_Sender_GRBL
 {
     public class Rectangle : Geometric
     {
-        public decimal MinorSide { get; set; }
-        public decimal PajorSide { get; set; }
+        public decimal Width { get; set; }
+        public decimal Length { get; set; }
+
+        public Point A
+        {
+            get
+            {
+                return new Point(Start.X, Start.Y + Length, Start.Z);
+            }
+        }
+
+        public Point B
+        {
+            get
+            {
+                return new Point(Start.X + Width, Start.Y + Length, Start.Z);
+            }
+        }
+
+        public Point C
+        {
+            get
+            {
+                return new Point(Start.X + Width, Start.Y, Start.Z);
+            }
+        }
     }
+
+    //public decimal Length { get; set; }
+
+
+    //A = X Y+l Z F
+    //B = X+w Y+l Z F
+    //C = X+w Y Z F
+    //D = X Y Z F
+
 }
