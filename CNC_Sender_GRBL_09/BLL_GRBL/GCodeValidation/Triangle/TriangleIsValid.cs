a4c464059ff0cda5e4a793bf362c1212170bb2a6
Gonzalo Rodriguez <gmrodriguez12@gmail.com>
2021-02-02 18:43:12 -0300

    triangle validation class

---
+++ CNC_Sender_GRBL_09/BLL_GRBL/GCodeValidation/Triangle/TriangleIsValid.cs
@@ -0,0 +1,17 @@
+﻿using DomainValidation.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL_GRBL.GCodeValidation.Triangle
+{
+    public class TriangleIsValid : Validator<ENT_Sender_GRBL.Circle>
+    {
+        public TriangleIsValid()
+        {
+
+        }
+    }
+}
