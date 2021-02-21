183360ce6195736b375459ce0bcf262484cfad63
Gonzalo Rodriguez <gmrodriguez12@gmail.com>
2021-02-20 21:06:44 -0300

    refactor triangle

--- CNC_Sender_GRBL_09/ENT_GRBL/TriangleRectangle.cs
+++ CNC_Sender_GRBL_09/ENT_GRBL/TriangleRectangle.cs
@@ -8,23 +8,23 @@ namespace ENT_Sender_GRBL
 {
     public class TriangleRectangle : Geometric
     {
-        public double Lenght { get; set; }
+        public double Base { get; set; }
         public double Height { get; set; }
 
         public Point A
         {
             get
             {
-                return new Point(Start.X + Lenght, Start.Y, Start.Z);
+                return new Point(Start.X + Base, Start.Y, Start.Z);
             }
         }
-
 
         public Point B
         {
             get
             {
-                return new Point(Start.X + Lenght, Start.Y + Height, Start.Z);
+                //return new Point(Start.X + Lenght, Start.Y + Height, Start.Z);
+                return new Point((Start.X + Base) / 2, Start.Y + Height, Start.Z);
             }
         }
 
